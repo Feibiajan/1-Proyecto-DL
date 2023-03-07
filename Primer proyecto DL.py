ba5f030016728a673bfa090d2540199b4e915181
Feibiajan <famavibo@estudiantec.cr>
2023-03-07 15:50:40 -0600

    Convertir en binario listo
    Los numeros que se ingresan, sean decimales o hexadecimales se convierten en binarios

--- Primer proyecto DL.py
+++ Primer proyecto DL.py
@@ -6,46 +6,60 @@ def decbin(n):
     return b[::-1]
 
 def hexdec(n):
-	numero = int(input(n))
-	convertido = hex(numero)[2:]
-	print(convertido)
+    print(n) 
+    n = int(n, base=16)
+    return n
 
 def Complemento2(n):
-        #Invertimos los unos y los ceros (Complemento a1):
-        for i in range(len(n)):
-            if n[i]==0:
-                n[i]=1
-            elif n[i]==1:
-                n[i]=0
+    #Invertimos los unos y los ceros (Complemento a1):
+    for i in range(len(n)):
+        if n[i]==0:
+            n[i]=1
+        elif n[i]==1:
+            n[i]=0
 
-        #Sumamos uno a la derecha (Complemento a2): 
-        for i in range(7,-1,-1):  #Recorre la lista de derecha a izquierda, empieza en el indice 7, va hasta el indice 0 y decrementa en 1
-            if n[i]==0:
-                n[i]=1
-                break
-            elif n[i]==1:
-                n[i]=0
-                
-bits=int(input("Seleccione numero de bit de los factores:"))
-print("Dijite un numero entre 0 y", (2**bits)-1)
-print("Digite antes del numero d:decimal, h:hexadecimal, b:binario")
-num1=input("Primer factor:")
-num2=input("Segundo factor:")
-fac1=list(num1)
-fac2=list(num2)
-x=fac1[1:]
-y=fac2[1:]
-print(x)
-print(y)
-z = [int(i) for i in x]
-o = [int(i) for i in y]
-print(z)
-print(o)
-pen=z[0:]
-print(pen)
-#for i in fac1:
-#    if 
-#   if num1==0 or num==2:
-#     print
-#print(,-,)
+    #Sumamos uno a la derecha (Complemento a2): 
+    for i in range(7,-1,-1):  #Recorre la lista de derecha a izquierda, empieza en el indice 7, va hasta el indice 0 y decrementa en 1
+        if n[i]==0:
+            n[i]=1
+            break
+        elif n[i]==1:
+            n[i]=0
+
+def main():                
+    bits=int(input("Seleccione numero de bit de los factores:"))
+    print("Dijite un numero entre 0 y", (2**bits)-1)
+    print("Digite antes del numero d:decimal, h:hexadecimal, b:binario")
+    num1=input("Primer factor:")
+    num2=input("Segundo factor:")
+    notacion_num1 = num1[0]
+    notacion_num2 = num2[0]
+    
+    num1 = switch_menu(notacion_num1, num1)
+    num2 = switch_menu(notacion_num2, num2)
+
+    print("binario factor 1",num1)
+    print("binario factor 2",num2)
+    
+        
+
+def switch_menu(notacion, num):
+    print("notacion: ", notacion, " numero: ", num)
+    if(notacion == 'b'):
+        return int(num[1:])
+    if(notacion == 'd'):
+        num = decbin(int(num[1:]))
+        return num
+    if(notacion == 'h'):
+        num = hexdec(num[1:])
+        num = decbin(num)
+        return num
+    if(notacion.isdigit()):
+        num = decbin(int(num))
+        return num
+    else:
+        print('Revise la notacion de los factores')
+
+main()
+
 
